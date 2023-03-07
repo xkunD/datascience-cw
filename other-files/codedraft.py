5620f3ef4cf7cef169081346a1f868b40dc79f14
xkunD <103330637+xkunD@users.noreply.github.com>
2023-03-07 20:30:49 +0000

    update section5

--- other-files/codedraft.py
+++ other-files/codedraft.py
@@ -30,10 +30,62 @@ def pretty_print_section_4(contact_dictionary):
     for sick_record in contact_dictionary:
         print(f"{sick_record} had contact with {format_list(contact_dictionary[sick_record])}")
 
+
+# Function for section 5
+def find_patients_zero_set(contacts_dic):
+    """Return list of people who do not appear in any sick person's contact
+    list. 
+
+    Args:
+        contacts_dic (dic): each entry is a sick person's name (key) and their
+        list of contacts.
+
+    Returns:
+        list: names of people who do not appear in any sick person's contact
+        list.
+    """
+    # Remove pass and fill in your code here
+    all_contacts = set(contacts_dic.keys())  # get all contacts in the keys
+    contacted_contacts = set().union(*contacts_dic.values())  # get all contacts that are contacted
+    missing_contacts = list(all_contacts - contacted_contacts)  # find the missing contacts
+    missing_contacts.sort()
+    print(missing_contacts)
+
+
+def find_patients_zero(contacts_dic):
+    sorted_contacts = []
+    patient_zero = []
+    
+    # iterate over each value list in sorted order, and add its
+    # contacts to the set of contacted contacts in a sorted manner
+    for contacts in contacts_dic.values():
+        i = 0
+        for name in contacts:
+            while i < len(sorted_contacts) and name > sorted_contacts[i]:
+                i += 1
+            if i == len(sorted_contacts) or name < sorted_contacts[i]:
+                sorted_contacts.insert(i, name)
+
+    # iterate over the sorted list of all contacts, and add any missing
+    # contacts to the missing_contacts list
+    i = 0
+    for name in contacts_dic.keys():
+        while i < len(sorted_contacts) and name > sorted_contacts[i]:
+            i += 1
+        if i == len(sorted_contacts) or name < sorted_contacts[i]:
+            patient_zero.append(name)
+
+    print(patient_zero)
+
+
+
+
+
 def main():
     #print(file_exists("sfadsf"))
     # print(parse_file("testfile.txt"))
-    pretty_print_section_4(parse_file("testfile.txt"))
+    # pretty_print_section_4(parse_file("testfile.txt"))
+    find_patients_zero(parse_file("DataSet1.txt"))
 
 if __name__ == '__main__':
     main()
